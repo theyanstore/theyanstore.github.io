646cc40733648bdacf71136a348f62ea8e6278b4
1111111 <103377744+frrrrrrqqfnlllflwe@users.noreply.github.com>
2024-08-10 11:41:08 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    setTimeout(function(){
+      document.querySelector('.loader-wrapper').style.display = 'none';
+    }, 3000);
+  });
+  
